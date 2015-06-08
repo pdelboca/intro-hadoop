f35af393f3ec85e5c69048adf35487a4c2d80d77
pdelboca <patriciodelboca@gmail.com>
2015-06-08 16:54:49 -0300

    Sales by Product

--- sales-by-category/mapper.py
+++ sales-by-category/mapper.py
@@ -10,4 +10,4 @@ for line in sys.stdin:
 
     if len(data) == 6:
         date, time, store, item, cost, payment = data
-        print "{0}\t{1}".format(store, cost)
+        print "{0}\t{1}".format(item, cost)
