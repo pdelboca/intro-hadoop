f54464aaa9dee51142fdcf2131c18d7cd2f47133
pdelboca <patriciodelboca@gmail.com>
2015-06-10 10:20:17 -0300

    Highest Individual Sale excercise

---
+++ highest-individual-sale/mapper.py
@@ -0,0 +1,13 @@
+# Your task is to make sure that this mapper code does not fail on corrupt data lines,
+# but instead just ignores them and continues working
+import sys
+
+
+# read standard input line by line
+for line in sys.stdin:
+    # strip off extra whitespace, split on tab and put the data in an array
+    data = line.strip().split("\t")
+
+    if len(data) == 6:
+        date, time, store, item, cost, payment = data
+        print "{0}\t{1}".format(store, cost)
---
+++ highest-individual-sale/reducer.py
@@ -0,0 +1,26 @@
+import sys
+
+max_sale = 0
+old_key = None
+
+for line in sys.stdin:
+    data = line.strip().split("\t")
+    if len(data) != 2:
+        # Something has gone wrong. Skip this line.
+        continue
+    
+    this_key, this_sale = data
+
+    # Detect categories changed so print
+    if old_key and this_key != old_key:
+        print old_key, "\t", max_sale
+        old_key = this_key
+        max_sale = 0
+    
+    if max_sale == 0 or max_sale < this_sale:
+        old_key = this_key
+        max_sale = this_sale
+
+# The last category will never enter the if condition
+if old_key != None:
+    print old_key, "\t", max_sale
