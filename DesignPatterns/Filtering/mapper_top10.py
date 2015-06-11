df8261b47b00937960c962f1d799552ce075979f
pdelboca <patriciodelboca@gmail.com>
2015-06-11 14:58:35 -0300

    Lesson 4 files. Filters and sample logs

---
+++ DesignPatterns/Filtering/mapper_top10.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+"""
+Your mapper function should print out 10 lines containing longest posts, sorted in
+ascending order from shortest to longest.
+Please do not use global variables and do not change the "main" function.
+"""
+import sys
+import csv
+import operator
+
+def mapper():
+    reader = csv.reader(sys.stdin, delimiter='\t')
+    writer = csv.writer(sys.stdout, delimiter='\t', quotechar='"', quoting=csv.QUOTE_ALL)
+
+    d = {}
+    for line in reader:
+
+        # YOUR CODE HERE
+        data = line[4]
+        
+        d[data] = len(data)
+    
+    sorted_list = sorted(d, key=d.get)[-10:]
+    for i in range(len(sorted_list)):
+        print '\"\"\t\"\"\t\"\"\t\"\"\t\"{0}\"\t\"\"'.format(sorted_list[i])
+
+
+
+test_text = """\"\"\t\"\"\t\"\"\t\"\"\t\"333\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"88888888\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"1\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"11111111111\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"1000000000\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"22\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"4444\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"666666\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"55555\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"999999999\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"7777777\"\t\"\"
+"""
+
+# This function allows you to test the mapper with the provided test string
+def main():
+    import StringIO
+    sys.stdin = StringIO.StringIO(test_text)
+    mapper()
+    sys.stdin = sys.__stdin__
+
+main()
