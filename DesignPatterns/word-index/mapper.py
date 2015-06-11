0fafd46b9aaa0832ad1ff40c0562d32d2ca37e48
pdelboca <patriciodelboca@gmail.com>
2015-06-11 16:21:23 -0300

    Added node list and case insensitive

--- DesignPatterns/word-index/mapper.py
+++ DesignPatterns/word-index/mapper.py
@@ -4,16 +4,16 @@ import csv
 header_line = True
 
 reader = csv.reader(sys.stdin, delimiter='\t')
-writer = csv.writer(sys.stdout, delimiter='\t', quotechar='"', quoting=csv.QUOTE_ALL)  
+#writer = csv.writer(sys.stdout, delimiter='\t', quotechar='"', quoting=csv.QUOTE_ALL)  
 
 for line in reader:
 
     if header_line:
         header_line = False
         continue
-
-    body = line[4]    
     
+    node = line[0]
+    body = line[4].lower()    
     characters = '.,!?:;"()<>[]#$=-/'
     for c in characters:
         body = body.replace(c,' ')
@@ -21,4 +21,4 @@ for line in reader:
     words = body.strip().split()
     
     for word in words:
-        print '{0}\t{1}'.format(word,1)
+        print '{0}\t{1}\t{2}'.format(word,1,node)
--- DesignPatterns/word-index/reducer.py
+++ DesignPatterns/word-index/reducer.py
@@ -3,17 +3,25 @@ import sys
  
 current_word = None
 current_count = 1
+node_list = []
  
 for line in sys.stdin:
-    word, count = line.strip().split('\t')
+    
+    word, count, node = line.strip().split('\t')
+
     if current_word:
         if word == current_word:
             current_count += int(count)
+            if node not in node_list:            
+                node_list.append(node)
         else:
-            print "{0}\t{1}".format(current_word, current_count)
+            print "{0}\t{1}\t{2}".format(current_word, current_count, node_list)
             current_count = 1
+            node_list = []
  
     current_word = word
+    if node not in node_list:            
+        node_list.append(node)
  
 if current_count > 1:
-    print "%s\t%d" % (current_word, current_count)
+    print "{0}\t{1}\t{2}".format(current_word, current_count, node_list)
