b38aa76e4f153340324060451bbd5524372c74df
pdelboca <patriciodelboca@gmail.com>
2015-06-15 16:43:37 -0300

    Added mean to the exercise

--- DesignPatterns/sales-mean/reducer.py
+++ DesignPatterns/sales-mean/reducer.py
@@ -6,6 +6,7 @@ import sys
  
 old_weekday = None
 total_sales = 0
+day_counts = 0
 
  
 reader = csv.reader(sys.stdin, delimiter='\t')
@@ -15,11 +16,13 @@ for line in reader:
     weekday, sales = line
 
     if old_weekday and old_weekday != weekday:
-        print "{0}\t{1}\t{2}".format(old_weekday, total_sales)
+        print "{0}\t{1}".format(old_weekday, total_sales/day_counts)
+        day_counts = 0
         total_sales = 0
         
  
     old_weekday = weekday
+    day_counts += 1
     total_sales += float(sales)
     
 if old_weekday:
