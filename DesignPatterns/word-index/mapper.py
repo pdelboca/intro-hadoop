e0b8cf9f4be7f16b783dc3c1217faf1ebc8e594b
pdelboca <patriciodelboca@gmail.com>
2015-06-15 14:12:38 -0300

    Map only strings with letters

--- DesignPatterns/word-index/mapper.py
+++ DesignPatterns/word-index/mapper.py
@@ -1,51 +1,24 @@
 #!/usr/bin/python
-#import sys
-#import csv
-#header_line = True
-#
-#reader = csv.reader(sys.stdin, delimiter='\t')
-##writer = csv.writer(sys.stdout, delimiter='\t', quotechar='"', quoting=csv.QUOTE_ALL)  
-#
-#for line in reader:
-#
-#    if header_line:
-#        header_line = False
-#        continue
-#
-#    if line[8]=="added_at":
-#        continue;
-#    
-#    node = line[0]
-#    body = line[4].lower()    
-#    characters = '.,!?:;"()<>[]#$=-/'
-#    for c in characters:
-#        body = body.replace(c,' ')
-#    
-#    words = body.strip().split()
-#    
-#    for word in words:
-#        print '{0}\t{1}\t{2}'.format(word,1,node)
-
 import sys
 import csv
-import re
-def mapper():
-    reader = csv.reader(sys.stdin, delimiter='\t')
-    delimiters = ['[',']','#','$','-','=','/',' ','\t','\n','.','!','?',':',';','\"','(',')','<','>',','];
-    regexPattern = '|'.join(map(re.escape, delimiters))
-    for line in reader:
-        #skip header..
-        if line[8]=="added_at":
-             continue;
-        node = line[0];
-        body = line[4];
-        words = re.split(regexPattern, body.lower(), 0)
-        for word in words:
-                if len(word)>0:
-                        print word, '\t', node;
-def main():
-    import StringIO
-    mapper()
+header_line = True
 
-if __name__ == "__main__":
-    main()
+reader = csv.reader(sys.stdin, delimiter='\t')
+
+for line in reader:
+
+    if header_line:
+        header_line = False
+        continue
+    
+    node = line[0]
+    body = line[4].lower()    
+    characters = '.,!?:;"()<>[]#$=-/'
+    for c in characters:
+        body = body.replace(c,' ')
+    
+    words = body.strip().split()
+    
+    for word in words:
+        if word.isalpha(): #print only strings containing letters        
+            print '{0}\t{1}\t{2}'.format(word,1,node)
--- DesignPatterns/word-index/reducer.py
+++ DesignPatterns/word-index/reducer.py
@@ -1,48 +1,23 @@
 #!/usr/bin/python
-#import sys
-# 
-#current_word = None
-#current_count = 1
-#node_list = []
-# 
-#for line in sys.stdin:
-#    
-#    word, count, node = line.strip().split('\t')
-#
-#    if current_word:
-#        if word == current_word:
-#            current_count += int(count)
-#            if node not in node_list:            
-#                node_list.append(node)
-#        else:
-#            print "{0}\t{1}\t{2}".format(current_word, current_count, node_list)
-#            current_count = 1
-#            node_list = []
-# 
-#    current_word = word
-#    if node not in node_list:            
-#        node_list.append(node)
-# 
-#if current_count > 1:
-#    print "{0}\t{1}\t{2}".format(current_word, current_count, node_list)
-
 import sys
-def reducer():
-    oldKey = None
-    array = []
-    for line in sys.stdin:
-        data = line.strip().split("\t");
-        thisKey, word = data;
-        if oldKey and oldKey != thisKey:
-                print oldKey,'\t','\t'.join(array)
-                array=[]
-        oldKey=thisKey
-        array.append(word)
-    if oldKey:
-        print oldKey,'\t','\t'.join(array)
-def main():
-    import StringIO
-    reducer()
+ 
+old_word = None
+current_count = 0
+node_list = []
+ 
+for line in sys.stdin:
+    
+    word, count, node = line.strip().split('\t')
 
-if __name__ == "__main__":
-    main()
+    if old_word and old_word != word:
+       print "{0}\t{1}\t{2}".format(old_word, current_count, node_list)
+       current_count = 0
+       node_list = []
+ 
+    old_word = word
+    current_count += 1
+    if node not in node_list:            
+        node_list.append(node)
+ 
+if old_word:
+    print "{0}\t{1}\t{2}".format(old_word, current_count, node_list)
