1e3d969a31ddd8b7857bc28291bf8d3339a0776a
pdelboca <patriciodelboca@gmail.com>
2015-06-15 15:00:56 -0300

    Added reader to reducer

--- DesignPatterns/word-index/reducer.py
+++ DesignPatterns/word-index/reducer.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python
+import csv
 import sys
  
 old_word = None
 current_count = 0
 node_list = []
  
-for line in sys.stdin:
+reader = csv.reader(sys.stdin, delimiter='\t')
+ 
+for line in reader:
     
-    word, count, node = line.strip().split('\t')
+    word, count, node = line
 
     if old_word and old_word != word:
-       print "{0}\t{1}\t{2}".format(old_word, current_count, node_list)
-       current_count = 0
-       node_list = []
+        print "{0}\t{1}\t{2}".format(old_word, current_count, node_list)
+        current_count = 0
+        node_list = []
  
     old_word = word
     current_count += 1
